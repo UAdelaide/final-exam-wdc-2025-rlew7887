c57eca931ef778c45139e33f1593956304d28342
rlew7887 <rachelchienwei.lew@student.adelaide.edu.au>
2025-06-20 13:44:40 +0000

    [automatic commit] 2025-06-20 13:44:39 UTC 165.228.176.10

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../models/db');
+const db = require('../db');
 
 // GET all users (for admin/testing)
 router.get('/', async (req, res) => {
