4eda992642c9d64c3253da5b1262ce38a80db1cf
rlew7887 <rachelchienwei.lew@student.adelaide.edu.au>
2025-06-20 13:43:15 +0000

    [automatic commit] 2025-06-20 13:43:14 UTC 165.228.176.10

--- part2/db.js
+++ part2/db.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
   host: 'localhost',
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  user: 'root',
+  password: '',
+  database: 'dogwalking',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
