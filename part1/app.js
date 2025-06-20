43b3b552bb554853ffb6a97b5952326a7c01a171
rlew7887 <rachelchienwei.lew@student.adelaide.edu.au>
2025-06-20 02:19:53 +0000

    [automatic commit] 2025-06-20 02:19:52 UTC 103.115.210.146

--- part1/app.js
+++ part1/app.js
@@ -53,10 +53,12 @@ let db;
     const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
     if (rows[0].count === 0) {
       await db.execute(`
-        INSERT INTO books (title, author) VALUES
-        ('1984', 'George Orwell'),
-        ('To Kill a Mockingbird', 'Harper Lee'),
-        ('Brave New World', 'Aldous Huxley')
+        INSERT INTO Users (username, email, password_hash, role)
+VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+('carol123', 'carol@example.com', 'hashed789', 'owner'),
+('sarahwalker', 'sarah@example.com', 'hashed111', 'walker'),
+('george123', 'george@example.com', 'hashed321', 'owner')
       `);
     }
   } catch (err) {
