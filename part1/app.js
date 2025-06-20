6e99a8285b91c1feb62512939f3bf16339a91728
rlew7887 <rachelchienwei.lew@student.adelaide.edu.au>
2025-06-20 04:06:23 +0000

    [automatic commit] 2025-06-20 04:06:23 UTC 165.228.176.10

--- part1/app.js
+++ part1/app.js
@@ -127,7 +127,7 @@ let db;
             (3, '2025-06-10 10:00:00', 45, 'Forks Trail', 'cancelled'),
             (4, '2025-06-09 08:30:00', 30, 'Central Park', 'completed'),
             (5, '2025-06-11 17:00:00', 60, 'Gardens', 'open'),
-            (1, '2025-06-09 08:00:00', 30, 'Parklands, 'completed')
+            (1, '2025-06-09 08:00:00', 30, 'Parklands', 'completed')
       `);
       await db.execute(`
         INSERT INTO WalkApplications (request_id, walker_id, status) VALUES
