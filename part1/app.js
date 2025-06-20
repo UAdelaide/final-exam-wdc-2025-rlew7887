dcfc1964d236dce7b8fa94cfcb8a997b4b63936b
rlew7887 <rachelchienwei.lew@student.adelaide.edu.au>
2025-06-20 03:54:55 +0000

    [automatic commit] 2025-06-20 03:54:55 UTC 165.228.176.10

--- part1/app.js
+++ part1/app.js
@@ -180,7 +180,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
 app.get('/api/walkers/summary', async (req, res) => {
     try {
         const [summary] = await db.execute(`
-            SELECT u.username AS walker_username, 
+            SELECT u.username AS walker_username, COUNT(r.rating)
             `);
         res.json(summary);
     } catch {
