45fa231bed492450720a3b9154557ae893a80d09
rlew7887 <rachelchienwei.lew@student.adelaide.edu.au>
2025-06-20 14:48:11 +0000

    [automatic commit] 2025-06-20 14:48:10 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out 165.228.176.10

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -66,7 +66,7 @@ router.post('/login', async (req, res) => {
 router.post('/logout', (req, res) => {
   req.session.destroy(err => {
     if (err) {
-      return res.status(500).json({ })
+      return res.status(500).json({ error: "Failed to logout' });
     }
   })
 });
