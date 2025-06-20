81abf49b85bf3295d82ec45e7fea7df45bb08c9a
rlew7887 <rachelchienwei.lew@student.adelaide.edu.au>
2025-06-20 02:33:31 +0000

    [automatic commit] 2025-06-20 02:33:30 UTC 103.115.210.146

--- part1/app.js
+++ part1/app.js
@@ -113,12 +113,12 @@ let db;
             ('george123', 'george@example.com', 'hashed321', 'owner')
       `);
       await db.execute(`
-        INSERT INTO Users (username, email, password_hash, role) VALUES
-            ('alice123', 'alice@example.com', 'hashed123', 'owner'),
-            ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-            ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-            ('sarahwalker', 'sarah@example.com', 'hashed111', 'walker'),
-            ('george123', 'george@example.com', 'hashed321', 'owner')
+        INSERT INTO Dogs (owner_id, name, size)
+VALUES ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
+((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
+((SELECT user_id FROM Users WHERE username = 'carol123'), 'Jacob', 'large'),
+((SELECT user_id FROM Users WHERE username = 'george123'), 'Milo', 'medium'),
+((SELECT user_id FROM Users WHERE username = 'george123'), 'Edward', 'medium')
       `);
       await db.execute(`
         INSERT INTO Users (username, email, password_hash, role) VALUES
