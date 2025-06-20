081a44fecfc325727cd04d06930e7817eed663f6
rlew7887 <rachelchienwei.lew@student.adelaide.edu.au>
2025-06-20 17:44:57 +0000

    [automatic commit] 2025-06-20 17:44:57 UTC 165.228.176.10

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -90,4 +90,18 @@ router.get('/dogs',async (req, res) => {
   }
 });
 
+// route to list all dogs
+router.get('/api/dogs', async (req, res) => {
+  try {
+    const [dogs] = await db.execute(`
+        SELECT d.name AS dog_name, d.size, u.username AS owner_username
+        FROM Dogs d
+        JOIN Users u on d.owner_id = u.user_id
+        `);
+    res.json(dogs);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch dogs' });
+  }
+});
+
 module.exports = router;
