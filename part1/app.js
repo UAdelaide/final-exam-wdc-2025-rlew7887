d6b9f097f7523556f45ca517f66c7f2d45ce81ae
rlew7887 <rachelchienwei.lew@student.adelaide.edu.au>
2025-06-20 03:21:47 +0000

    [automatic commit] 2025-06-20 03:21:47 UTC 165.228.176.10

--- part1/app.js
+++ part1/app.js
@@ -150,7 +150,8 @@ let db;
 app.get('/api/dogs', async (req, res) => {
   try {
     const [dogs] = await db.execute('
-        SELECT * FROM Dogs');
+        SELECT d.name AS dog
+        ');
     res.json(dogs);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch dogs' });
