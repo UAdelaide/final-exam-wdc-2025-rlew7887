eeed293bd38e23ce0431ce53f87675c803a5aa87
rlew7887 <rachelchienwei.lew@student.adelaide.edu.au>
2025-06-20 02:42:40 +0000

    [automatic commit] 2025-06-20 02:42:39 UTC 103.115.210.146

--- part1/app.js
+++ part1/app.js
@@ -138,7 +138,7 @@ let db;
       `);
       await db.execute(`
         INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments) VALUES
-            ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+            (4, 2, 'alice123', 'alice@example.com', 'hashed123', 'owner'),
             ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
             ('carol123', 'carol@example.com', 'hashed789', 'owner'),
             ('sarahwalker', 'sarah@example.com', 'hashed111', 'walker'),
